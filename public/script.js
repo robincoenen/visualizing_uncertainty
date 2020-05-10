a2653fc4fc9ed7f019f530ddfb6b46dff761c097
EC2 Default User <ec2-user@ip-172-31-62-14.ec2.internal>
2020-05-10 17:27:50 +0000

    build frontend

--- public/script.js
+++ public/script.js
@@ -4,17 +4,30 @@ function myFunction(){
         console.log('get new results')
         let result = data*100
         $('#demo').html(`<b>${result.toFixed(2)}</b>`)
-        if (result < 10){
-            $("#demo").css("color","blue")
-         } else if (result < 20) {
-            $("#demo").css("color","red") 
-         } else if (result < 30) {
-            $("#demo").css("color","orange") 
-        } else {
-            $("#demo").css("color","black")
+        if (result < 3){
+            $("#myImage").attr("src","1-3.gif");
+         } else if (result < 5) {
+            $("#myImage").attr("src","3-5.gif"); 
+         } else if (result < 7) {
+            $("#myImage").attr("src","5-7.gif"); 
+         } else if (result < 9) {
+            $("#myImage").attr("src","7-9.gif"); 
+         }  else if (result < 91) {
+            $("#myImage").attr("src","sonst_1.gif"); 
+         }else if (result < 93) {
+            $("#myImage").attr("src","91-93.gif"); 
+         } else if (result < 95) {
+            $("#myImage").attr("src","93-95.gif"); 
+         } else if (result < 97) {
+            $("#myImage").attr("src","95-97.gif"); 
+         } else if (result < 100) {
+            $("#myImage").attr("src","97-100.gif"); 
+         }  else {
+            $("#myImage").attr("src","status.png"); 
         }
     });
 }
 
 
 
+//.attr("src","second.jpg");
